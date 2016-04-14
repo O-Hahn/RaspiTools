a9f12fb41289b395456af3b3d3ff060cb99b9262
Olaf Hahn <olaf_hahn@at.ibm.com>
2016-04-14 19:59:35 +0200

    hostname

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RaspiTools
 Raspi Tools
+
+This toolset helps to display the boot/IP-Adress on the Grovepi-LCD-Display connected to I2C
+
+please check before if python-netifaces are installed
+
+sudo apt-get install python-netifaces
--- ipaddr.py
+++ ipaddr.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python
 
 import netifaces as ni
+import socket as so
+
 from grove_lcd import *
 
+hostname = so.gethostname() 
 ints = ni.interfaces()
 for i in ints:
   if 'eth' in i:
     try:
       ni.ifaddresses(i)[2][0]['addr']
       setColor("blue")
-      print("ETH Address:" + ni.ifaddresses(i)[2][0]['addr'])
-      setText("IP Address:#" + ni.ifaddresses(i)[2][0]['addr'])
+      print(hostname + " ETH Address:" + ni.ifaddresses(i)[2][0]['addr'])
+      setText(hostname + "#" + ni.ifaddresses(i)[2][0]['addr'])
     except:
       pass
   elif 'wlan' in i:
     try:
       ni.ifaddresses(i)[2][0]['addr']
       setColor("blue")
-      print("WLAN Address: " + ni.ifaddresses(i)[2][0]['addr'])
-      setText("IP Address:#" + ni.ifaddresses(i)[2][0]['addr'])
+      print(hostname + " WLAN Address: " + ni.ifaddresses(i)[2][0]['addr'])
+      setText(hostname + "#" + ni.ifaddresses(i)[2][0]['addr'])
     except:
       pass
